a3a5a443f0a44ad54fade6f9b4f6c8aa110489be
Landon Manning <lmanning17@gmail.com>
2015-10-24 13:12:00 -0300

    Whoops!
    Accidentally committed this bugger.

--- _assets/reference_theme/default/BGAnimations/ScreenWithMenuElements background/default.lua
+++
@@ -1,5 +0,0 @@
-return Def.ActorFrame {
-	LoadActor( "bg" ) .. {
-		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y);
-	};
-};
--- _assets/reference_theme/default/Graphics/ComboGraph ComboNumber.lua
+++
@@ -1,3 +0,0 @@
-return LoadFont("Common Normal") .. {
-	InitCommand=cmd(shadowlengthx,0;shadowlengthy,2;strokecolor,color("#00000077"););
-};
